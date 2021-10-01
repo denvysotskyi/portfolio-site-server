f322694577baaa9a2fdb5d5b885f3d61e0d7033c
denvysotskyi <denvysotskyi@gmail.com>
2021-10-01 17:29:04 +0300

    Modify main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 (async () => {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(process.env.PORT || 7777);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  await app.listen(7777);
 })();
